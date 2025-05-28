d6fc84ca40544ae10c947a0b024bbe4d0be5c2f0
lycheejam <32588863+Lycheejam@users.noreply.github.com>
2025-05-28 14:46:11 +0000

    私は久しぶりにVPCを作ったのでインターネットに繋がる経路を作るのを忘れていましたの札を下げています。

--- terraform/main.tf
+++ terraform/main.tf
@@ -137,6 +137,25 @@ resource "aws_vpc" "this" {
   enable_dns_hostnames = true
 }
 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_route" "this" {
+  route_table_id         = aws_route_table.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "this" {
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.this.id
+}
+
 resource "aws_subnet" "this" {
   vpc_id            = aws_vpc.this.id
   cidr_block        = "10.0.0.0/24"
@@ -146,6 +165,12 @@ resource "aws_subnet" "this" {
 resource "aws_security_group" "this" {
   name   = "ecs-scheduled-task-example"
   vpc_id = aws_vpc.this.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "this" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
 }
 
 resource "aws_iam_role" "eventbridge_ecs_scheduled_task_role" {
